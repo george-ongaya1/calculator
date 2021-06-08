7ccd62b05aed3fa6f04e43f84ba09c5547caeff4
George-ongaya1git config --global user.email georgeongaya94.gmail.comgit config --golabl user.name George-ongaya1 <georgeongaya94@gmail.com>
2021-06-08 12:33:20 +0300

    added a function expression divide in js.js

--- js/scripts.js
+++ js/scripts.js
@@ -23,4 +23,11 @@ var number2= parseInt(prompt("Enter another number:"));
 var result=subtract(number1,number2);
 alert(result);*/
 
- 
+ var division = function (number1,number2){
+     return number1/ number2;
+ };
+ var number1 = parseInt(prompt("enter a number"));
+ var number2 = parseInt(prompt("enter another number"));
+ var result = division(number1, number2);
+ alert(division);
+
